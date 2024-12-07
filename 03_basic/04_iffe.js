b9c5b210b3158127e5d493ba6ce4c6dd518a7e34
adnan1103 <adnan.yousaf726@gmail.com>
2024-12-07 18:30:31 +0500

    iife update new commit

--- 03_basic/04_iffe.js
+++ 03_basic/04_iffe.js
@@ -2,12 +2,19 @@
 
 // Simple Function
 // function chai(){
-//     console.log(`DB Connected`);   
+//     console.log(`DB Connected`);
 // }
 // chai()
 
-
 // IIFE for immediate execution and removal of global execution
-(function chai(){
-    console.log(`DB Connected`);   
-})()
+
+// iife function  end on semicolan
+// This is named iife like chai
+(function chai() {
+  console.log(`DB Connected`);
+})();
+
+// This is without named iife
+((name) => {
+  console.log(`DB Connected Two Statements ${name}`);
+})("Adnan");
